c3081a622ba371bbe38ee75a4c98403ac296e11a
Jorge Seijas <jseijasg22@fpcoruna.afundacion.org>
2023-10-18 13:35:13 +0200

    Creado main.php sin enlaces funcionales.

--- sprint2sql/main.php
+++ sprint2sql/main.php
@@ -1,4 +1,12 @@
 <html>
+
+<head>
+	<style>
+		h2 {
+			color: red;
+		}
+	</style>
+</head>
 <body>
 
 <?php
@@ -6,5 +14,22 @@
 ?>
 
 	<h1>Conexion establecida</h1>
+	<h2>Datos de la tabla tJuegos</h2>
+	<ul>
+	<?php
+	$query = 'SELECT * FROM tJuegos';
+	$result = mysqli_query($db, $query) or die('Query error');
+	while ($row = mysqli_fetch_array($result)){
+		echo '<li>';
+		echo '<a href = "/detail.php?id='.$row['id'].'">'.$row['id'].': '.$row['nombre'].'</a>';
+		echo '<br>';
+		echo '<img src = "'.$row['url_imagen'].'" alt = "Imagen" width = "300px" height = "200px">';
+		echo '<br>';
+		echo $row['anyo'].', '.$row['genero'];
+		echo '</li>';
+		echo '<br>';
+	}
+	?>
+	</ul>
 </body>
 </html>
