e1f142a961aa6b5616fbbdd3b78323cafa51487e
Jorge Seijas <jseijasg22@fpcoruna.afundacion.org>
2023-10-18 14:14:08 +0200

    Creado comment.php

--- sprint2sql/detail.php
+++ sprint2sql/detail.php
@@ -28,5 +28,12 @@
 	mysqli_close($db);
 	?>
 </ul>
+<br>
+<p>Deja un comentario nuevo:</p>
+<form action = "/comment.php" method = "post">
+	<textarea rows = "4" cols = "50" name = "new_comment"></textarea><br>
+	<input type = "hidden" name = "juego_id" value = "<?php echo $juego_id; ?>">
+	<input type = "submit" value = "Comentar">
+</form>
 </body>
 </html>
