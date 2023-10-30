50c5cdabd4172b298f6f1d34794adb3500444609
JorgeSeijas <145001194+JorgeSeijas@users.noreply.github.com>
2023-10-30 13:33:00 +0100

    Add files via upload
    Ejercicio 1 adivinanza

---
+++ adivinanza.py
@@ -0,0 +1,11 @@
+print("""No muerde ni ladra, pero tiene dientes y la casa guarda. ¿Qué es?
+      a. Puerta
+      b. Ventana
+      c. Llave
+""")
+respuesta=input("Escribe la opción correcta.")
+while respuesta != "c":
+    print("Casi lo tienes...")
+    respuesta=input("Escribe la opción correcta.")
+print("Correcto!!!")
+    
