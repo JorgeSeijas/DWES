d7546833884d009306a91dcfbe633731062cdc4c
JorgeSeijas <145001194+JorgeSeijas@users.noreply.github.com>
2023-10-31 14:16:40 +0100

    adivinanza.py actualizado

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,47 +1,46 @@
-puntuacion = 0 # Variable para almacenar la puntuación
-print("""No muerde ni ladra, pero tiene dientes y la casa guarda. ¿Qué es?
-    a. Puerta
-    b. Ventana
-    c. Llave
-""")
-respuesta=input("Escribe la opción correcta.")
-while (respuesta!="a") & (respuesta != "b") & (respuesta != "c"): # Bucle para comprobar que lo que introduzca el usuario es una opción válida
-    respuesta = input ("Opciones válidas (a, b, c)") 
-if respuesta != "c":    # Respuesta incorrecta --> -5 puntos 
-    print("Casi lo tienes...")
-    puntuacion = puntuacion - 5
-else:
-    print("Correcto!!!") # Respuesta correcta --> +10 puntos 
-    puntuacion = puntuacion + 10
+import random    
 
-print("""\nPico de cuerno, ala de ave, la rodilla para atrás, y anda adelante. 
-    a. Gallina
-    b. Avestruz
-    c. Pingüino
-""")
-respuesta=input("Escribe la opción correcta.")
-while (respuesta!="a") & (respuesta != "b") & (respuesta != "c"):
-    respuesta = input ("Opciones válidas (a, b, c)") 
-if respuesta != "a":
-    print("Casi lo tienes...")
-    puntuacion = puntuacion - 5
-else:
-    print("Correcto!!!")
-    puntuacion = puntuacion + 10
+if __name__ == "__main__":
+    puntuacion = 0 # Variable para almacenar la puntuación
 
-print("""\nNieto de tu bisabuelo, padre de tus hermanos, de tus primos es el tío y de tus tíos hermano.
-    a. Abuelo
-    b. Padre
-    c. Tio
-""")
-respuesta=input("Escribe la opción correcta.")
-while (respuesta!="a") & (respuesta != "b") & (respuesta != "c"):
-    respuesta = input ("Opciones válidas (a, b, c)") 
-if respuesta != "b":
-    print("Casi lo tienes...")
-    puntuacion = puntuacion - 5
-else:
-    print("Correcto!!!")
-    puntuacion = puntuacion + 10
+    adivinanza_1 = {
+        'enunciado': "No muerde ni ladra, pero tiene dientes y la casa guarda. ¿Qué es?",
+        'opciones': "a. Puerta\nb. Ventana\nc. Llave",
+        'correcta': "c"
+    }
+    adivinanza_2 = {
+        'enunciado': "Pico de cuerno, ala de ave, la rodilla para atrás, y anda adelante.",
+        'opciones': "a. Gallina\nb. Avestruz\nc. Pingüino",
+        'correcta': "a"
+    }
+    adivinanza_3 = {
+        'enunciado': "Nieto de tu bisabuelo, padre de tus hermanos, de tus primos es el tío y de tus tíos hermano.",
+        'opciones': "a. Abuelo\nb. Padre\nc. Tio",
+        'correcta': "b"
+    }
+    adivinanzas = [adivinanza_1, adivinanza_2, adivinanza_3]    #Lista de posibles adivinanzas
+    usada = []      #Lista para descartar las adivinanzas usadas
+    elegida = {}    #Adivinanza elegida
+    vuelta = 0      #Variable de control
+    for i in range(2) :   
+        elegida = random.choice(adivinanzas)    #eleccion de adivinanza
+        usada.append(elegida['enunciado'])
+        while (elegida['enunciado'] == usada[0]) & (vuelta == 1):   #Comprobación para no ser repetida, solo se activa en la 2º vuelta
+            usada = []
+            elegida = {}
+            elegida = random.choice(adivinanzas)
+            usada.append(elegida)
+        vuelta = 1
+        print(elegida['enunciado'])     #Presentación
+        print(elegida['opciones'])
+        res=input("Escribe la opción correcta.")
+        while (res!="a") & (res != "b") & (res != "c"): # Bucle para comprobar que lo que introduzca el usuario es una opción válida
+            res = input ("Opciones válidas (a, b, c)") 
+        if (res != elegida['correcta']):
+            print("Casi lo tienes...\n\n")
+            puntuacion = puntuacion - 5
+        else:
+            print("Correcto!!!\n\n")
+        puntuacion = puntuacion + 10
 
-print("\nPuntuación total: "+ str(puntuacion))
+    print("\nPuntuación total: "+ str(puntuacion))
