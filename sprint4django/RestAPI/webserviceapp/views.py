80cdb802e95c6dc24bde9b787173a84a53d74e5b
Jorge Seijas <jseijasg22@fpcoruna.afundacion.org>
2023-11-21 13:23:14 +0100

    Segundo endpoint

--- sprint4django/RestAPI/webserviceapp/views.py
+++ sprint4django/RestAPI/webserviceapp/views.py
@@ -12,7 +12,27 @@ def devolver_juegos(request):
         diccionario = {}
         diccionario['id'] = fila_sql.id
         diccionario['nombre'] = fila_sql.nombre
+        diccionario['url_imagen'] = fila_sql.url_imagen
         diccionario['anyo'] = fila_sql.anyo
         diccionario['genero'] = fila_sql.genero
         respuesta_final.append(diccionario)
     return JsonResponse(respuesta_final, safe = False)
+
+def devolver_juego_por_id(request, id_solicitado):
+    juego = Tjuegos.objects.get(id = id_solicitado)
+    comentarios = juego.tcomentarios_set.all()
+    lista_comentarios = []
+    for fila_comentario_sql in comentarios:
+        diccionario = {}
+        diccionario['id'] = fila_comentario_sql.id
+        diccionario['comentario'] = fila_comentario_sql.comentario
+        lista_comentarios.append(diccionario)
+    resultado = {
+        'id': juego.id,
+        'nombre': juego.nombre,
+        'url_imagen': juego.url_imagen,
+        'año': juego.anyo,
+        'genero': juego.genero,
+        'comentarios': lista_comentarios
+    }
+    return JsonResponse(resultado, json_dumps_params={'ensure_ascii': False})
